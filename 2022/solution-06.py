6798a36bad3abdb550ea246d5a103e593dc1712d
sim1029 <sps67@pitt.edu>
2022-12-06 16:48:17 -0500

    Day 6 solution

--- 2022/solution-06.py
+++ 2022/solution-06.py
@@ -1,9 +1,28 @@
+from collections import deque
+
 def solution_06(option):
-    input = open("Inputs/input-05.txt")
+    input = open("Inputs/input-06.txt")
     curr = input.readline().rstrip()
-    while curr:
-        print(curr)
-        curr = input.readline().rstrip()
+    seen = []
+    msg_marker = []
+    first = 0
+    first_msg = 0
+    for i, c in enumerate(curr):
+        if len(set(seen)) >= 4 and first == 0:
+            first = i
+        if len(set(msg_marker)) >= 14:
+            first_msg = i
+            break
+        if len(seen) > 3:
+            seen.pop(0)
+        seen.append(c)
+        if len(msg_marker) > 14:
+            msg_marker.pop(0)
+        msg_marker.append(c)
+    print(first)
+    print(first_msg)
+
+
 
 
 if __name__ == "__main__":
